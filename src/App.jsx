b98f6fba1b9559cd69087f85f14da7e2bc113bdf
mickael keita <keitamickael@gmail.com>
2024-12-18 11:52:16 +0100

    integrating mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "./assets/images/icon-sun.svg";
+import cross from "./assets/images/icon-cross.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="main">
+      <div className="container">
+        <div className="head">
+          <h1>Todo</h1>
+          <img src={logo} alt="logo" />
+        </div>
+        <form className="add-todo" action="">
+          <div className="circle"></div>
+          <input type="text" placeholder="Create new todo..." />
+        </form>
+        <div className="todo-container">
+          <div className="todo-item">
+            <div>
+              <div className="circle"></div>
+              <p>Read for 1 hour</p>
+            </div>
+            <img src={cross} alt="cross" />
+          </div>
+          <div className="todo-item">
+            <div>
+              <div className="circle"></div>
+              <p>10 minutes meditations</p>
+            </div>
+            <img src={cross} alt="cross" />
+          </div>
+          <div>
+            <p>5 items left</p>
+            <p>Clear Completed</p>
+          </div>
+        </div>
+        <ul className="todo-filter">
+          <li>All</li>
+          <li>Active</li>
+          <li>Completed</li>
+        </ul>
+        <p className="dndText">Drag and drop to reorder list</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
